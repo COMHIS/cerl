bd2dd3aa2583aabf9ed2b957c8d978e7fcec91e8
antagomir <leo.lahti@iki.fi>
2016-03-23 12:58:18 +0100

    ok

--- digihum.R
+++ digihum.R
@@ -1,32 +1,26 @@
+## ----digihum-1, echo=FALSE, message=FALSE, warning=FALSE, out.width="155px", gig.width=5, fig.height=5----
 dfs <- df %>% group_by(author) %>% tally() %>% arrange(desc(n)) %>% filter(!is.na(author))
 
-# ------------------------
-
-write_xtable(df$author, file = "~/tmp/topauthors.csv")
-
-# ------------------------
-
+#write_xtable(df$author, file = "~/tmp/topauthors.csv")
 
 top.authors <- dfs$author[1:2]
 
 dfs <- df %>% filter(author %in% top.authors) %>% group_by(country) %>% tally() %>% arrange(desc(n)) %>% filter(!is.na(country))
-top.countries <- dfs$country[1:6]
+top.countries <- dfs$country
 
 dfs <- df %>% filter(author %in% top.authors & country %in% top.countries) %>% group_by(country, author, publication_decade) %>% tally() %>% arrange(publication_decade)
-
-# -------------------------------
 
 # Top-2 authors in their top-6 countries
 
 theme_set(theme_bw(20))
 pics <- list()
 countries <- unique(dfs$country)
-for (id in countries) {
+for (id in countries[1:12]) {
   dfss <- dfs %>% filter(country == id) %>% group_by(author)
 
   dfss$author <- factor(dfss$author, levels = levels(dfs$author))
 
-  pics[[id]] <- ggplot(dfss, aes(x = publication_decade, y = n, fill = author)) +
+  p <- ggplot(dfss, aes(x = publication_decade, y = n, fill = author)) +
        geom_bar(stat = "identity", position = "stack", color = "black") +
        xlab("Publication Decade") +
        ylab("Title Count") +
@@ -35,13 +29,12 @@ for (id in countries) {
        #guides(fill = guide_legend("Author")) 
        ggtitle(id) +
        xlim(c(range(dfs$publication_decade)))
+
+  print(p)
+
 }
 
-library(gridExtra)
-grid.arrange(pics[[1]], pics[[2]], pics[[3]],
-	     pics[[4]], pics[[5]], pics[[6]], nrow = 2)
 
 
-# ------------------------------
 
 
